b8d9252164b6ee84302e493927bc411f6d891b0b
Ebil Abilli <ebilebilli3gmail.com>
2025-10-03 11:22:31 +0400

    ShopMedia model created

--- shop_service/shops/v1/models/shop_media_model.py
+++ shop_service/shops/v1/models/shop_media_model.py
@@ -1,4 +1,23 @@
 from django.db import models
 
+from v1.models import Shop
 
-# class ShopMedia(models.Model):
+
+class ShopMedia(models.Model):
+    shop = models.ForeignKey(
+        Shop
+    )
+    image = models.ImageField(
+        upload_to='shop_media',
+        null=False,
+        verbose_name='images of shop'
+    )
+    alt_text = models.CharField(
+        max_length=255, 
+        blank=True, 
+        null=True,
+        verbose_name='alt text for image'
+        )
+
+    def __str__(self):
+        return self.id
