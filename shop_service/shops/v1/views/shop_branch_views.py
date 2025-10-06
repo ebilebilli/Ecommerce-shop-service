091a515c8fc12094e5f7202a72ce7f08ef91d992
Ebil Abilli <ebilebilli3gmail.com>
2025-10-06 09:51:43 +0400

    ShopBranchDetailAPIView created.Slug field added to ShopBrnach model,new signal created for ShopBranch model

--- shop_service/shops/v1/views/shop_branch_views.py
+++ shop_service/shops/v1/views/shop_branch_views.py
@@ -11,7 +11,7 @@ from ..serializers.shop_branch_serializer import ShopBranchSerializer
 from utils.pagination import CustomPagination
 
 
-class ShopBrancListByShopAPIView(APIView):
+class ShopBranchListByShopAPIView(APIView):
     permission_classes = [AllowAny]
     http_method_names = ['get']
 
@@ -26,3 +26,13 @@ class ShopBrancListByShopAPIView(APIView):
             {'detail': 'No active branches found for this shop.'}, 
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class ShopBranchDetailAPIView(APIView):
+    permission_classes = [AllowAny]
+    http_method_names =['get']
+   
+    def get(self, request, shop_branch_slug):
+        shop_branch = get_object_or_404(ShopBranch, slug=shop_branch_slug, is_active=True)
+        serializer = ShopBranchSerializer(shop_branch)
+        return Response(serializer.data, status=status.HTTP_200_OK)
