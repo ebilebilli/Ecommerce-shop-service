c579ccb7944bcdd61652ed3f31d807b0cc6ecc09
Ebil Abilli <ebilebilli3gmail.com>
2025-10-06 09:46:32 +0400

    ShopBrancListByShopAPIView created.This api brings shop branchs by shop

---
+++ shop_service/shops/v1/views/shop_branch_views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from ..models.shop_model import Shop
+from ..models.shop_branch_model import ShopBranch
+from ..serializers.shop_branch_serializer import ShopBranchSerializer
+from utils.pagination import CustomPagination
+
+
+class ShopBrancListByShopAPIView(APIView):
+    permission_classes = [AllowAny]
+    http_method_names = ['get']
+
+    def get(self, request, shop_slug):
+        shop = get_object_or_404(Shop, slug=shop_slug, is_active=True)
+        shop_branchs = ShopBranch.objects.filter(shop=shop, is_active=True)
+        if shop_branchs.exists():
+            serializer = ShopBranchSerializer(shop_branchs, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(
+            {'detail': 'No active branches found for this shop.'}, 
+            status=status.HTTP_400_BAD_REQUEST
+        )
