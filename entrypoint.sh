8bc64eab8ce936eea1e253032384cdc2785af603
Ebil Abilli <ebilebilli3gmail.com>
2025-10-08 11:06:40 +0400

    docker-compose and docker file created but not tested yet.Some internet pronlems :/

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+set -e
+
+# Apply database migrations
+python shop_service/manage.py migrate --noinput
+
+# Collect static files (safe if none exist)
+python shop_service/manage.py collectstatic --noinput --clear
+
+# Start server
+python shop_service/manage.py runserver 0.0.0.0:8000
