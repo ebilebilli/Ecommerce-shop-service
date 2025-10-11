8dcca1f21e978d4a14cced7feb9cc036406813a6
Ebil Abilli <ebilebilli3gmail.com>
2025-10-11 23:19:33 +0400

    allowed host problem solved

--- shop_service/shop_service/settings.py
+++ shop_service/shop_service/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
