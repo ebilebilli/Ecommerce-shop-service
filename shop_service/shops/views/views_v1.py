867636b8201f5bdf311a73a515fb2195faf27cc0
Ebil Abilli <ebilebilli3gmail.com>
2025-10-14 22:27:12 +0400

    changes for gateway tests

--- shop_service/shops/views/views_v1.py
+++ shop_service/shops/views/views_v1.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -59,7 +61,7 @@ class ShopDetailAPIView(APIView):
         serializer = ShopDetailSerializer(shop)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
+@method_decorator(csrf_exempt, name='dispatch')
 class CreateShopAPIView(APIView):
     """Create a new shop. Only authenticated users can create."""
     #permission_classes = [IsAuthenticated]
