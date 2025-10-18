2cc30d65437cbabd7d6aea3fde8a46c8a4a64d9b
Ebil Abilli <ebilebilli3gmail.com>
2025-10-18 12:54:30 +0400

    middleware logging created

--- shop_service/shop_service/middleware.py
+++ shop_service/shop_service/middleware.py
@@ -1,6 +1,8 @@
 import logging
+from django.http import HttpResponse
 
 logger = logging.getLogger('shop_service')
+
 
 class DRFLoggingMiddleware:
     def __init__(self, get_response):
@@ -10,11 +12,22 @@ class DRFLoggingMiddleware:
         logger.info(f'Request: {request.method} {request.get_full_path()}')
         logger.info(f'Headers: {dict(request.headers)}')
         if request.body:
-            logger.info(f"Body: {request.body.decode('utf-8')}")
+            try:
+                logger.info(f"Body: {request.body.decode('utf-8')}")
+            except Exception:
+                logger.info("Body: <unreadable>")
 
         response = self.get_response(request)
-        logger.info(f'Response status: {response.status_code}')
-        if hasattr(response, 'data'):
-            logger.info(f'Response data: {response.data}')
-        else:
-            logger.info(f'Response content: {response.content.decode("utf-8")}')
+        if response is None:
+            response = HttpResponse()
+
+        logger.info(f'Response status: {getattr(response, "status_code", "N/A")}')
+        try:
+            if hasattr(response, 'data'):
+                logger.info(f'Response data: {response.data}')
+            else:
+                logger.info(f'Response content: {getattr(response, "content", b"").decode("utf-8")}')
+        except Exception:
+            logger.info("Response content: <unreadable>")
+
+        return response
